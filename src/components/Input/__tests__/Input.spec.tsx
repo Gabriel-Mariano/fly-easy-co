f5fec15c1d6ce4a334859b376f63ce205b04d463
Gabriel Mariano Oliveira <gabrielmarianooliveira@MacBook-Pro-de-Gabriel.local>
2024-02-27 12:53:10 -0300

    feat(InputComponent): Criado testes unitário para o component

--- src/components/Input/__tests__/Input.spec.tsx
+++ src/components/Input/__tests__/Input.spec.tsx
@@ -1,3 +1,4 @@
+import { Text } from "react-native";
 import { fireEvent, render } from "@testing-library/react-native";
 import { Input } from "../index";
 
@@ -21,5 +22,34 @@ describe("Input Component", ()=> {
     it("should not render with label when label prop is not provided", ()=> {
         const { queryByText } = render(<Input />);
         expect(queryByText("some-label")).toBeNull();
+    });
+    it("should render with contentLeft when contentLeft prop is provided", ()=> {
+        const { getByText } = render(<Input contentLeft={<Text>Left</Text>}/>);
+        expect(getByText("Left")).toBeOnTheScreen();
+    });
+    it("should not render with contentLeft when contentLeft prop is not provided", ()=> {
+        const { queryByText } = render(<Input />);
+        expect(queryByText("Left")).toBeNull();
+    });
+    it("should render with contentRight when contentRight prop is provided", ()=> {
+        const { getByText } = render(<Input contentRight={<Text>Right</Text>}/>);
+        expect(getByText("Right")).toBeOnTheScreen();
+    });
+    it("should not render with contentRight when contentRight prop is not provided", ()=> {
+        const { queryByText } = render(<Input />);
+        expect(queryByText("Right")).toBeNull();
+    });
+    it("should apply styles conditionally based on contentLeft and contentRight props", ()=> {
+        const { getByTestId } = render(<Input contentLeft={<Text>Left</Text>} testID="input-id"/>);
+        expect(getByTestId("input-id")).toHaveStyle({ width:"88%" });
+    });
+    it("should apply styles conditionally if contentLeft and contentRight is provided", ()=> {
+        const { getByTestId } = render(
+            <Input 
+                contentLeft={<Text>Left</Text>} 
+                contentRight={<Text>Right</Text>}
+                testID="input-id"/>
+            );
+        expect(getByTestId("input-id")).toHaveStyle({ width:"76%" });
     });
 })
