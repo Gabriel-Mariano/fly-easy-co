8888fe9833b19b4933c7b99378d8f26c82d0ad4f
Gabriel Mariano Oliveira <gabrielmarianooliveira@MacBook-Pro-de-Gabriel.local>
2024-03-22 13:22:01 -0300

    feat(StatusBar): Inserido o statusBar nas Rotas Autenticadas

--- src/routes/authenticated/stack/index.tsx
+++ src/routes/authenticated/stack/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Header } from '@src/components/Header';
 import { HomeScreen } from '@src/views/authenticated/home';
+import { Colors } from '@src/themes';
+import { StatusBar } from 'react-native';
 
 type RootStackParam = {
     Home:undefined,
@@ -9,10 +12,20 @@ type RootStackParam = {
 const { Navigator, Screen } = createNativeStackNavigator<RootStackParam>();
 
 function AuthenticatedRoutes() {
+   
     return (
+        <>
+        <StatusBar translucent barStyle={'light-content'} backgroundColor={Colors.primary} />
         <Navigator>
-            <Screen name="Home" component={HomeScreen} />
+            <Screen 
+                name="Home" 
+                component={HomeScreen} 
+                options={{
+                    header:()=> <Header />
+                }}
+            />
         </Navigator>
+        </>
     )
 }
 
